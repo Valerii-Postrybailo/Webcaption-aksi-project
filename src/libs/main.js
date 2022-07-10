114eabe4509f3c7b00486bc6b6e465e341ce72e1
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-07-10 04:22:55 +0300

    slick slider

---
+++ src/libs/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  
+  $('.reviews-list').slick()({
+    // arrows:false,
+    // dots: true,
+    // infinite: true,
+    // slidesToShow: 2,
+    // slidesToScroll: 1
+  })
+
+});
