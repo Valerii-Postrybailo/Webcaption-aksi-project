56104aa9715c9a783cf4fff961d932dba30dfdc3
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-07-16 03:10:59 +0300

    slider cards

--- src/libs/main.js
+++ src/libs/main.js
@@ -1,11 +1,32 @@
 $(function () {
   
-  $('.reviews-list').slick()({
-    // arrows:false,
-    // dots: true,
-    // infinite: true,
-    // slidesToShow: 2,
-    // slidesToScroll: 1
-  })
+  $('.reviews-list').slick({
+    arrows:false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
 
+    responsive: [
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  })
 });
