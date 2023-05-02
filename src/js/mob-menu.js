4ab2e3cf111c72f212715182fb07a4aaa638b937
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2023-05-02 23:16:10 +0300

    mobile menu done

---
+++ src/js/mob-menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+      openMenuBtn: document.querySelector(".menu-open-btn"),
+      closeMenuBtn: document.querySelector(".mob-menu-close-btn"),
+      menu: document.querySelector("[data-menu]"),
+
+      mobMenuItems: document.querySelectorAll(".mob-nav__list-item")
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.mobMenuItems.forEach((item)  => item.addEventListener("click", toggleMenu))
+
+  function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+  }
+})();
